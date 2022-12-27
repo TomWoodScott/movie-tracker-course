96219cf0826d29299d683ae44e1e8ba78b717bbe
Thomas Woodland-Scott <thomaswoodland-scott@hotmail.com>
2022-12-27 17:54:25 +0000

    Add CRUD endpoints

--- api/handlers/demo.py
+++ api/handlers/demo.py
@@ -1,8 +1,21 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, Query, Path, Body
+from pydantic import BaseModel
 from api.responses.detail import DetailResponse
+from starlette.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+
 
 router = APIRouter(prefix="/api/v1/demo")
+
+# GET - Retrieve data.
+# POST - To send data.
+# PUT, PATCH - To update data.
+# DELETE - To delete data.
+
+# it is important to keep endpoints stateless.
+class NameIn(BaseModel):
+    name: str
+    prefix: str = "Mr"
 
 
 @router.get("/", response_model=DetailResponse)
@@ -11,3 +24,47 @@ def hello_world():
     This is the hello world end point
     """
     return DetailResponse(message="Hello World")
+
+
+@router.get("/hello", response_model=DetailResponse)
+def send_date_query(name: str = Query(..., title="User", description="The name")):
+    return DetailResponse(message="Hello")
+
+
+@router.post("/hello/name", response_model=DetailResponse)
+def send_data_body(name: NameIn = Body(..., title="Body", description="This is some description")):
+    """
+    Response with Hello name, where name is user provided.
+    """
+    return DetailResponse(message=f"Hello, {name.prefix}. {name.name}")
+
+
+@router.post("/hello/{name}", response_model=DetailResponse)
+def send_data_body(name: str = Path(..., title="Name")):
+    """
+    Response with Hello name, where name is user provided.
+    """
+    return DetailResponse(message=f"Hello, {name}")
+
+
+@router.delete(
+    "/delete/{name}",
+    response_model=DetailResponse,
+    responses={404: {"model": DetailResponse}},
+)
+def delete_data(name: str):
+    if name == "admin":
+        raise JSONResponse(
+            status_code=404,
+            content=jsonable_encoder(
+                DetailResponse(message="cannot delete data for admin")
+            )
+        )
+    return DetailResponse(message=f"Data deleted for {name}")
+
+# we choose not to use HTTPException as it gives us less adaptable error messages. 
+# @router.delete(
+#     "/error", response_model=DetailResponse, responses={404: {"model": DetailResponse}}
+# )
+# def get_error_http():
+#     raise HTTPException(404, detail="This endpoint always fails.") 
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import os
-
 import uvicorn
 
 from api.api import create_app
